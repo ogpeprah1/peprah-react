3a26415770220a64152d5bd2b3c4474e853f5c8a
ogpeprah1 <102240482+ogpeprah1@users.noreply.github.com>
2024-02-26 23:12:39 +0000

    under dev

--- src/components/service-component.jsx
+++ src/components/service-component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ServiceCard(props) {
   const { title, description, img } = props;
   return (
-    <div className="rounded-md bg-black py-4 px-4 w-[full] md:w-[40%] flex gap-4">
+    <div className="rounded-md bg-black py-4 px-4 w-[full] md:w-[40%] flex gap-5">
       <img src={`${img}`} alt="" className="bg-green-400 h-full basis-[30%]" />
       <div className="basis-[70%]">
         <h1 className="text-white text-2xl md:text-3xl font-bold mb-4">
